e2011e7961de8ca40e0200cef490e75ad67ef325
marckeelingiv <32782256+marckeelingiv@users.noreply.github.com>
2024-05-30 22:26:24 +0000

    added domain code

--- 🏠_Homepage.py
+++ 🏠_Homepage.py
@@ -1,4 +1,8 @@
 import streamlit as st
+## import environment variables
+from os import getenv
+
+google_adds_link = getenv("GOOGLE_ADDS_LINK")
 
 st.set_page_config(
     page_title="Financial Calculators",
@@ -9,3 +13,7 @@ st.set_page_config(
 
 st.title("Financial Calculators")
 st.sidebar.success("Welcome to the Financial Calculators!")
+
+custom_html = f'<script async src="{google_adds_link}" crossorigin="anonymous"></script>'
+
+st.markdown(custom_html, unsafe_allow_html=True)
