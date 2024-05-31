d0ca6140951cd07e72d784a92a6b9dfa08df31c4
marckeelingiv <32782256+marckeelingiv@users.noreply.github.com>
2024-05-31 18:05:04 +0000

    trying javascript way

--- app_resources/app_functions.py
+++ app_resources/app_functions.py
@@ -1,33 +1,19 @@
-import streamlit as st
 import os
-from bs4 import BeautifulSoup
-import pathlib
-
+import streamlit as st
 # Get the Google Adds link from environment variables
 google_adds_link = os.getenv("GOOGLE_ADDS_LINK")
 
 def add_script_to_header():
+    add_script_js = f"""
+    <script>
+    (function() {{
+        var script = document.createElement('script');
+        script.src = '{google_adds_link}';
+        script.crossOrigin = 'anonymous';
+        script.async = true;
+        document.head.appendChild(script);
+    }})();
+    </script>
     """
-    Function to add script tag to the head tag of an HTML file
-    """
-    filename = pathlib.Path(st.__file__).parent / "static" / "index.html"
-    # Open the HTML file and parse it into a BeautifulSoup object
-    with open(filename, 'r') as file:
-        soup = BeautifulSoup(file, 'html.parser')
-
-    # Create a new script tag
-    new_tag = soup.new_tag(  
-        name='script',  
-        attrs={  
-            'async': '',
-            'src': google_adds_link,  
-            'crossorigin': 'anonymous',  
-            }  
-    )  
-
-    # Add the new script tag to the head tag
-    soup.head.append(new_tag)
-
-    # Write the modified HTML back to the file
-    with open(filename, 'w') as file:
-        file.write(str(soup))
+    
+    st.markdown(add_script_js, unsafe_allow_html=True)
